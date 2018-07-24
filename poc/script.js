0428ad7105fe9c13cc6ee040ae781aac4bf22c20
Lajos.Meszaros <m_lajos@hotmail.com>
2018-07-24 23:21:34 +0200

    Created dummy code, which shows how I would like to use the repo

--- poc/script.js
+++ poc/script.js
@@ -13,54 +13,90 @@ we need an internal vocabulary of all the used nodes and the context
   multiple contexts?
 */
 
-const virtualContext = {}
+class VirtualAudioContext{
+  constructor() {
+    this.destination = null
+    this.patch = {}
+  }
+  createOscillator() {
+    return {
+      frequency: {
+        value: 440
+      },
+      connect: target => {
 
-const refs = {
-  oscillators: {},
-  gains: {},
-  context: null
+      },
+      start: () => {
+
+      }
+    }
+  }
+  createGain() {
+    return {
+      gain: {
+        value: 1
+      },
+      connect: target => {
+
+      }
+    }
+  }
+}
+
+const diff = (virtualCtxA, virtualCtxB) => {
+  const patchA = virtualCtxA.patch
+  const patchB = virtualCtxB.patch
+}
+const patch = (patch, ctx) => {
+
+}
+const render = (virtualCtx, ctx) => {
+  patch(virtualCtx.patch, ctx)
 }
 
 // -------------
 
-// creating the audio app
-
-const ctx = new AudioContext()
-
-const osc = ctx.createOscillator()
-const gain = ctx.createGain()
-
-osc.frequency.value = 200
-
-gain.gain.value = 0.5
-
-osc.connect(gain)
-gain.connect(ctx.destination)
-
-osc.start()
-
-// modifying the app:
-
-setTimeout(() => {
-  gain.gain.value = 0
-}, 1000)
-
-// the modification should be configured this way:
-
-/*
-setTimeout(() => {
-  const ctx = new AudioContext()
+const create = () => {
+  const ctx = new VirtualAudioContext()
 
   const osc = ctx.createOscillator()
   const gain = ctx.createGain()
 
   osc.frequency.value = 200
 
-  gain.gain.value = 0 // note the difference
+  gain.gain.value = 0.5
 
   osc.connect(gain)
   gain.connect(ctx.destination)
 
   osc.start()
+
+  return ctx
+}
+
+const modify = () => {
+  const ctx = new VirtualAudioContext()
+
+  const osc = ctx.createOscillator()
+  const gain = ctx.createGain()
+
+  osc.frequency.value = 200
+
+  gain.gain.value = 0 // this is the line, that changed
+
+  osc.connect(gain)
+  gain.connect(ctx.destination)
+
+  osc.start()
+
+  return ctx
+}
+
+const a = create()
+const b = modify()
+const ctx = new AudioContext()
+
+render(a, ctx)
+setTimeout(() => {
+  patch(diff(a, b), ctx) // should turn off the gain's volume
 }, 1000)
-*/
