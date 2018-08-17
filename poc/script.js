65f9ab5ff144490eb0eacc67f63e4e2060d8ef7c
Lajos Meszaros <m_lajos@hotmail.com>
2018-08-17 16:32:36 +0200

    Added monosynth with manual volume lfo

--- poc/script.js
+++ poc/script.js
@@ -299,6 +299,7 @@ const render = (virtualCtx, ctx) => {
 
 // -------------
 
+/*
 const create = () => {
   const ctx = new VirtualAudioContext()
 
@@ -344,3 +345,137 @@ setTimeout(() => {
   console.log('a second later:')
   patch(diff(a, b), ctx) // should turn off the gain's volume
 }, 1000)
+*/
+
+const sound = (frequency, volume) => {
+  const ctx = new VirtualAudioContext()
+
+  const osc = ctx.createOscillator()
+  const gain = ctx.createGain()
+
+  osc.frequency.value = frequency
+
+  gain.gain.value = volume
+
+  osc.connect(gain)
+  gain.connect(ctx.destination)
+
+  osc.start()
+
+  return ctx
+}
+
+const frequencies = [
+  261.6,
+  277.1,
+  293.6,
+  311.1,
+  329.6,
+  349.2,
+  369.9,
+  391.9,
+  415.3,
+  440,
+  466.1,
+  493.8,
+  523.2
+]
+const keyMap = {
+  A: 0,
+  W: 1,
+  S: 2,
+  E: 3,
+  D: 4,
+  F: 5,
+  T: 6,
+  G: 7,
+  Z: 8,
+  Y: 8,
+  H: 9,
+  U: 10,
+  J: 11,
+  K: 12
+}
+
+const ctx = new AudioContext()
+
+let old = null
+
+const change = (virtualCtx, ctx) => {
+  if (old === null) {
+    render(virtualCtx, ctx)
+  } else {
+    patch(diff(old, virtualCtx), ctx)
+  }
+
+  old = virtualCtx
+}
+
+let lastPressedNoteIndex = null
+let isNotePlaying = false
+let masterVolume = 1
+const keyPressed = {
+  A: false,
+  W: false,
+  S: false,
+  E: false,
+  D: false,
+  F: false,
+  T: false,
+  G: false,
+  Z: false,
+  Y: false,
+  H: false,
+  U: false,
+  J: false,
+  K: false
+}
+
+document.body.addEventListener('keydown', e => {
+  const pressedChar = String.fromCharCode(e.keyCode)
+  if (!keyPressed[pressedChar]) {
+    keyPressed[pressedChar] = true
+
+    if (keyMap.hasOwnProperty(pressedChar)) {
+      if (lastPressedNoteIndex !== keyMap[pressedChar] || !isNotePlaying) {
+        lastPressedNoteIndex = keyMap[pressedChar]
+        isNotePlaying = true
+
+        change(sound(frequencies[lastPressedNoteIndex], masterVolume), ctx)
+      }
+    }
+  }
+})
+
+document.body.addEventListener('keyup', e => {
+  const pressedChar = String.fromCharCode(e.keyCode)
+  if (keyPressed[pressedChar]) {
+    keyPressed[pressedChar] = false
+
+    if (keyMap.hasOwnProperty(pressedChar)) {
+      if (lastPressedNoteIndex === keyMap[pressedChar] && isNotePlaying) {
+        lastPressedNoteIndex = keyMap[pressedChar]
+        isNotePlaying = false
+        change(sound(frequencies[lastPressedNoteIndex], 0), ctx)
+      }
+    }
+  }
+})
+
+let lfoDirection = 1
+setInterval(() => {
+  if (lfoDirection === 1) {
+    masterVolume += 0.1
+    if (masterVolume > 0.9) {
+      lfoDirection = -1
+    }
+  } else {
+    masterVolume -= 0.1
+    if (masterVolume < 0.1) {
+      lfoDirection = 1
+    }
+  }
+  if (isNotePlaying) {
+    change(sound(frequencies[lastPressedNoteIndex], masterVolume), ctx)
+  }
+}, 50)
