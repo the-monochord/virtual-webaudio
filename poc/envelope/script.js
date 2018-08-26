90b77ba23ee8293e53393b107d45c33ff6df240e
Lajos.Meszaros <m_lajos@hotmail.com>
2018-08-26 13:26:39 +0200

    docs(poc): Added envelope example
    though it's not working yet with the VirtualAudioContext

---
+++ poc/envelope/script.js
@@ -0,0 +1,30 @@
+/* global virtualWebaudio, AudioContext */
+
+const { VirtualAudioContext, patch, render, diff } = virtualWebaudio
+
+const attackAndDecay = () => {
+  const ctx = new VirtualAudioContext()
+
+  const osc = ctx.createOscillator()
+  const gain = ctx.createGain()
+
+  osc.frequency.value = 400
+
+  gain.gain.setValueAtTime(0, ctx.currentTime)
+  gain.gain.linearRampToValueAtTime(1, ctx.currentTime + 1)
+
+  gain.gain.setValueAtTime(1, ctx.currentTime + 2)
+  gain.gain.linearRampToValueAtTime(0, ctx.currentTime + 3)
+
+  osc.connect(gain)
+  gain.connect(ctx.destination)
+
+  osc.start()
+
+  return ctx
+}
+
+const note = attackAndDecay()
+const ctx = new AudioContext()
+
+render(note, ctx)
