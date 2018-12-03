5729b83b857f5684843cbe475f72d095531d4653
Lajos.Meszaros <m_lajos@hotmail.com>
2018-12-03 22:38:19 +0100

    chore(package): Updated ramda

--- src/VirtualAudioParam.js
+++ src/VirtualAudioParam.js
@@ -19,13 +19,13 @@ class VirtualAudioParam {
   }
 
   setValueAtTime (...args) {
-    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'setValueAtTime'], this._.nodeId, this._.ctx.currentTime, adjust(markTimeArg, 1, args))
+    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'setValueAtTime'], this._.nodeId, this._.ctx.currentTime, adjust(1, markTimeArg, args))
   }
   linearRampToValueAtTime (...args) {
-    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'linearRampToValueAtTime'], this._.nodeId, this._.ctx.currentTime, adjust(markTimeArg, 1, args))
+    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'linearRampToValueAtTime'], this._.nodeId, this._.ctx.currentTime, adjust(1, markTimeArg, args))
   }
   exponentialRampToValueAtTime (...args) {
-    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'exponentialRampToValueAtTime'], this._.nodeId, this._.ctx.currentTime, adjust(markTimeArg, 1, args))
+    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'exponentialRampToValueAtTime'], this._.nodeId, this._.ctx.currentTime, adjust(1, markTimeArg, args))
   }
   setTargetAtTime (...args) {
     this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'setTargetAtTime'], this._.nodeId, this._.ctx.currentTime, args)
@@ -34,10 +34,10 @@ class VirtualAudioParam {
     this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'setValueCurveAtTime'], this._.nodeId, this._.ctx.currentTime, args)
   }
   cancelScheduledValues (...args) {
-    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'cancelScheduledValues'], this._.nodeId, this._.ctx.currentTime, adjust(markTimeArg, 0, args))
+    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'cancelScheduledValues'], this._.nodeId, this._.ctx.currentTime, adjust(0, markTimeArg, args))
   }
   cancelAndHoldAtTime (...args) {
-    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'cancelAndHoldAtTime'], this._.nodeId, this._.ctx.currentTime, adjust(markTimeArg, 0, args))
+    this._.ctx._.events.add(EVENTS.CALL, [this._.name, 'cancelAndHoldAtTime'], this._.nodeId, this._.ctx.currentTime, adjust(0, markTimeArg, args))
   }
 }
 
